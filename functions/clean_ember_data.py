745d7668ea1860bd97da526c3773fdeba69a2825
L. Wiersma <larswiersma@icloud.com>
2024-07-01 17:35:08 +0200

    Updated to standard Datetime format, no separate columns.

--- functions/clean_ember_data.py
+++ functions/clean_ember_data.py
@@ -6,13 +6,9 @@ def clean_ember_data(filename):
     df = pd.read_csv(filename)
     df['Datetime (Local)'] = pd.to_datetime(df['Datetime (Local)'])
     df = df[df['Datetime (Local)'].dt.year == 2023]
-    df = df.assign(
-        Month = df['Datetime (Local)'].dt.month,
-        Day = df['Datetime (Local)'].dt.day,
-        Hour = df['Datetime (Local)'].dt.hour).drop(
+    df = df.drop(
             ['ISO3 Code',
-             'Datetime (UTC)',
-             'Datetime (Local)'],
+             'Datetime (UTC)'],
              axis=1)
 
     return df
